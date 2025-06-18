90a1a960305169355b238c0a9dba47ad7d406d10
Juan Patrcio da Silva Borges <juanpatriciodasilvaborges1@gmail.com>
2025-06-18 14:00:45 -0300

    Implementamos um botão de adicionar

--- gui.py
+++ gui.py
@@ -2,9 +2,24 @@ import funções
 import FreeSimpleGUI as sg
 
 rotulo = sg.Text("Adicionar tarefa: ")
-caixa_entrada = sg.InputText(tooltip="Digite a tarefa: ")
-botao_add = sg.Button("Adicionar: ")
+caixa_entrada = sg.InputText(tooltip="Digite a tarefa: ", key="lista")
+botao_add = sg.Button("Adicionar")
 
-janela = sg.Window("Lista de tarefas", layout=[[rotulo], [botao_add, caixa_entrada]])
-janela.read()
+janela = sg.Window("Lista de tarefas",
+                   layout=[[rotulo], [botao_add, caixa_entrada]],
+                   font=('Helvetica', 15))
+
+while True:
+    evento, valores = janela.read()
+    print(evento)
+    print(valores)
+    match evento:
+        case "Adicionar":
+            lista = funções.leitura()
+            new_entrada = valores["lista"] + "\n"
+            lista.append(new_entrada)
+            funções.registrar(lista)
+        case sg.WINDOW_CLOSED:
+            break
+
 janela.close()
